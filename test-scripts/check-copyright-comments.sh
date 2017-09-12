f095ad8bb230dd8f039619caf3a5af0bbe044e7e
Joshua Liebow-Feeser <hello@joshlf.com>
2017-09-12 12:42:45 -0700

    test-scripts/check-copyright-comments.sh: Validate modification years

--- test-scripts/check-copyright-comments.sh
+++ test-scripts/check-copyright-comments.sh
@@ -6,6 +6,18 @@
 # Licensed under the Apache License, Version 2.0 (the LICENSE file). This file
 # may not be copied, modified, or distributed except according to those terms.
 
+# last_modification_year <file>
+function last_modification_year {
+  COMMIT=$(git log --pretty=format:"%H" "$1" | head -n 1)
+  git show -s --format=%ci "$COMMIT" | cut -d - -f 1
+}
+
+# last_modification_year <file>
+function first_modification_year {
+  COMMIT=$(git log --pretty=format:"%H" "$1" | tail -n 1)
+  git show -s --format=%ci "$COMMIT" | cut -d - -f 1
+}
+
 # like check_comments, checks the <line>th line for the comment
 #
 # check_comments_shell <file extension> <comment prefix> <line>
@@ -16,9 +28,25 @@ function check_comments_line {
   # 'while read file', which spawns a subshell, and thus prevents
   # us from setting the RET variable from inside the loop.
   for file in $(find . -name "*${1}"); do
-    head -n "$3" "$file" | tail -n 1 | grep "$2" >/dev/null
+    # Don't check files that aren't in version control
+    if ! git ls-files --error-unmatch "$file" >/dev/null 2>/dev/null; then
+      continue
+    fi
+
+    # We're either looking for a comment of the form 'Copyright 2017'
+    # or of the form 'Copyright 2017-2018'.
+    FIRST_YEAR=$(first_modification_year "$file")
+    LAST_YEAR=$(last_modification_year "$file")
+    YEAR=$(if [ "$FIRST_YEAR" == "$LAST_YEAR" ]; then
+      echo "$FIRST_YEAR"
+    else
+      echo "${FIRST_YEAR}-${LAST_YEAR}"
+    fi)
+    COMMENT="$2 Copyright $YEAR"
+
+    head -n "$3" "$file" | tail -n 1 | grep "$COMMENT" >/dev/null
     if [ $? -ne 0 ]; then
-      echo "$file: missing copyright comment" >&2
+      echo "$file: missing or malformed copyright comment" >&2
       RET=1
     fi
   done
@@ -32,9 +60,9 @@ function check_comments {
 }
 
 EXIT=0
-check_comments '.rs' '// Copyright' || EXIT=1
-check_comments '.md' '<!-- Copyright' || EXIT=1
-check_comments '.toml' '# Copyright' || EXIT=1
+check_comments '.rs' '//' || EXIT=1
+check_comments '.md' '<!--' || EXIT=1
+check_comments '.toml' '#' || EXIT=1
 # In shell scripts, the copyright comment should start on line 3
-check_comments_line '.sh' '# Copyright' 3 || EXIT=1
+check_comments_line '.sh' '#' 3 || EXIT=1
 exit "$EXIT"
