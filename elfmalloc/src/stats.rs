daff68e2b78f77f4f3e4f4a9bea387c9e9109631
Eli Rosenthal <ezrosenthal@gmail.com>
2017-09-21 00:37:36 -0700

    elfmalloc: Silence warnings

--- elfmalloc/src/stats.rs
+++ elfmalloc/src/stats.rs
@@ -22,8 +22,6 @@
 //! during thread initialization time because TLS can call `calloc`. This is similar to a problem
 //! encountered in the `general::global` in this crate. `RefCell`'s `try_borrow` gives us a guard
 //! against this recursion.
-use std::cell::RefCell;
-use std::sync::atomic::{AtomicUsize, Ordering};
 
 type Num = i64;
 
@@ -55,28 +53,32 @@ pub struct AllocStats {
     pub grabbed_clean: Num,
 }
 
-lazy_static! {
-    pub static ref THREAD_CTR: AtomicUsize = AtomicUsize::new(0);
-}
+#[cfg(feature = "print_stats")]
+pub mod state {
+    use super::*;
+    use std::cell::RefCell;
+    use std::sync::atomic::{AtomicUsize, Ordering};
+    pub struct StatsHandle {
+        pub stats: RefCell<AllocStats>,
+        pub thread_num: usize,
+    }
+    lazy_static! {
+        pub static ref THREAD_CTR: AtomicUsize = AtomicUsize::new(0);
+    }
+    thread_local! {
+        pub static LOCAL_STATS: StatsHandle = StatsHandle {
+            stats: RefCell::new(AllocStats::default()),
+            thread_num: THREAD_CTR.fetch_add(1, Ordering::Relaxed),
+        };
+    }
 
-#[allow(dead_code)]
-pub struct StatsHandle {
-    pub stats: RefCell<AllocStats>,
-    pub thread_num: usize,
-}
-
-thread_local! {
-    pub static LOCAL_STATS: StatsHandle = StatsHandle {
-        stats: RefCell::new(AllocStats::default()),
-        thread_num: THREAD_CTR.fetch_add(1, Ordering::Relaxed),
-    };
 }
 
 macro_rules! trace_event {
     ($fld:tt) => {
         #[cfg(feature = "print_stats")]
         {
-            let _ = super::stats::LOCAL_STATS.try_with(|sh| {
+            let _ = $crate::stats::state::LOCAL_STATS.try_with(|sh| {
                 if let Ok(mut _f_ref) = sh.stats.try_borrow_mut() {
                     _f_ref.$fld += 1;
                     let allocs = _f_ref.slag_alloc + _f_ref.cache_alloc;
