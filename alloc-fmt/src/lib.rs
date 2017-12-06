b5146d603ff3fa50090bda5e3f57593552dd095b
Michael Sullivan <icecreamman1917@gmail.com>
2017-12-06 14:00:43 -0800

    alloc-fmt: Fix Windows compilation bug
    Fixes #124

--- alloc-fmt/src/lib.rs
+++ alloc-fmt/src/lib.rs
@@ -68,7 +68,11 @@ impl Write for FDWriter {
         let mut buf = s.as_bytes();
         while !buf.is_empty() {
             unsafe {
+                #[cfg(not(windows))]
                 let written = libc::write(self.0, buf.as_ptr() as *const _, buf.len());
+                #[cfg(windows)]
+                let written =
+                    libc::write(self.0, buf.as_ptr() as *const _, buf.len() as libc::c_uint);
                 if written < 1 {
                     core::intrinsics::abort();
                 }
