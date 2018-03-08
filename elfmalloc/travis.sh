76fea51eda227f25dc54214e766247ec32d5d66b
Joshua Liebow-Feeser <hello@joshlf.com>
2018-03-08 13:45:29 +0300

    elfmalloc: Re-enable Travis tests, set RUST_TEST_THREADS=1
    - Re-enable Travis tests and set RUST_TEST_THREADS=1. We've previously
      had an issue with running out of memory during Travis tests, so
      this commit forces the tests to run sequentially to reduce the
      maximum working set size.

--- elfmalloc/travis.sh
+++ elfmalloc/travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2017 the authors. See the 'Copyright and license' section of the
+# Copyright 2017-2018 the authors. See the 'Copyright and license' section of the
 # README.md file at the top-level directory of this repository.
 #
 # Licensed under the Apache License, Version 2.0 (the LICENSE-APACHE file) or
@@ -10,13 +10,20 @@
 set -x
 set -e
 
-# Skip elfmalloc tests until we can get them working (still build so we can
-# detect compilation failures).
-travis-cargo --only nightly build
-exit 0
+# Since elfmalloc tests use a lot of memory, we use RUST_TEST_THREADS=1
+# to force them to run sequentially in order to reduce the total memory
+# consumption and avoid a crash on Travis.
 
 travis-cargo --only nightly build
-RUST_BACKTRACE=1 travis-cargo --only nightly test
-for feature in prime_schedules huge_segments no_lazy_region nightly; do
-  RUST_BACKTRACE=1 travis-cargo --only nightly test -- --features "$feature"
+RUST_TEST_THREADS=1 RUST_BACKTRACE=1 travis-cargo --only nightly test \
+  -- --features low-memory-tests
+for feature in    \
+  prime_schedules \
+  huge_segments   \
+  no_lazy_region  \
+  local_cache     \
+  magazine_layer  \
+  c-api; do
+  RUST_TEST_THREADS=1 RUST_BACKTRACE=1 travis-cargo --only nightly test \
+    -- --features "$feature low-memory-tests"
 done
